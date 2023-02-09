9c025109bb98245eaaa4579725b82d988594492e
RARimonAhmed <rayhanahmedrimon44@gmail.com>
2023-02-09 12:26:32 +0600

    Data is succefully sent in firebase

---
+++ firebase_send_data/lib/views/homepage.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future sendData() async {
+    final data = FirebaseFirestore.instance;
+    data.collection("UserInformation").add({
+      'Name': 'Bahar',
+      'Age': 22.toString(),
+      'CGPA': 3.15.toString(),
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Send Data To Cloud'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: (() {
+                sendData();
+              }),
+              child: const Text('Send Data')),
+        ],
+      )),
+    ));
+  }
+}
